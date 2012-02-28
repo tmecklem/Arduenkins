a54f0b4343e2ef466d49ddc89ea08a0eed42eef4
Timothy Mecklem <timothy@mecklem.com>
2012-02-28 09:18:08 -0500

    Brandon told me it needed more techno, and he's "The SME"

--- libraries/ShiftBriteM/Animations.c
+++ libraries/ShiftBriteM/Animations.c
@@ -3,9 +3,9 @@
 #define pi 3.14 //IS EXACTLY 3!
 
 void pulseAnimation(const uint16_t referenceColor[], uint16_t *currentStep, uint8_t lightUpdateFrequency, uint16_t colorsForCommand[], int *finished) {
-  colorsForCommand[0] =  (int)(( ( sin( pi/2 + (float)(*currentStep) * pi / lightUpdateFrequency ) +1 ) / 2 ) * (float)referenceColor[0]);
-  colorsForCommand[1] =  (int)(( ( sin( pi/2 + (float)(*currentStep) * pi / lightUpdateFrequency ) +1 ) / 2 ) * (float)referenceColor[1]);
-  colorsForCommand[2] =  (int)(( ( sin( pi/2 + (float)(*currentStep) * pi / lightUpdateFrequency ) +1 ) / 2 ) * (float)referenceColor[2]);
+  colorsForCommand[0] =  (int)(( ( sin( pi/2 + (float)(*currentStep) * 2 * pi / lightUpdateFrequency ) +1 ) / 2 ) * (float)referenceColor[0]);
+  colorsForCommand[1] =  (int)(( ( sin( pi/2 + (float)(*currentStep) * 2 * pi / lightUpdateFrequency ) +1 ) / 2 ) * (float)referenceColor[1]);
+  colorsForCommand[2] =  (int)(( ( sin( pi/2 + (float)(*currentStep) * 2 * pi / lightUpdateFrequency ) +1 ) / 2 ) * (float)referenceColor[2]);
   *currentStep = (*currentStep) % (lightUpdateFrequency*2);
 }
 
